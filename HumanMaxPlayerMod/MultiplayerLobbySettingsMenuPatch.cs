3b755874dd3fe2dbc186938c06740141ed0835ed
Denis Suchok <35270280+DeNcHiK3713@users.noreply.github.com>
2023-08-10 23:25:47 +0300

    Update MultiplayerLobbySettingsMenuPatch.cs

--- HumanMaxPlayerMod/MultiplayerLobbySettingsMenuPatch.cs
+++ HumanMaxPlayerMod/MultiplayerLobbySettingsMenuPatch.cs
@@ -10,33 +10,30 @@ using TMPro;
 
 namespace HumanMaxPlayerMod
 {
-    public class MultiplayerLobbySettingsMenuPatch
+    [HarmonyPatch(typeof(MultiplayerLobbySettingsMenu), nameof(MultiplayerLobbySettingsMenu.OnGotFocus))]
+    public static class MultiplayerLobbySettingsMenu_OnGotFocus
     {
-        [HarmonyPatch(typeof(MultiplayerLobbySettingsMenu), nameof(MultiplayerLobbySettingsMenu.OnGotFocus))]
-        public static class MultiplayerLobbySettingsMenu_OnGotFocus
+        public static bool Prefix(MenuSelector ___maxPlayersSelector)
         {
-            public static bool Prefix(MenuSelector ___maxPlayersSelector)
+            var count = byte.MaxValue - 1;
+            if (___maxPlayersSelector.optionLabels.Length != count)
             {
-                var count = byte.MaxValue - 1;
-                if (___maxPlayersSelector.optionLabels.Length != count)
+                var optionLabel = ___maxPlayersSelector.optionLabels.First();
+                var right = optionLabel.transform.parent.Find("Right");
+                right.SetParent(null);
+                var newLabels = Enumerable.Range(___maxPlayersSelector.optionLabels.Length + 2, count - ___maxPlayersSelector.optionLabels.Length).Select(x =>
                 {
-                    var optionLabel = ___maxPlayersSelector.optionLabels.First();
-                    var right = optionLabel.transform.parent.Find("Right");
-                    right.SetParent(null);
-                    var newLabels = Enumerable.Range(___maxPlayersSelector.optionLabels.Length + 2, count - ___maxPlayersSelector.optionLabels.Length).Select(x =>
-                    {
-                        var newOptionLabel = UnityEngine.Object.Instantiate(optionLabel, optionLabel.transform.parent, false);
-                        newOptionLabel.name = x.ToString();
-                        newOptionLabel.text = x.ToString();
-                        return newOptionLabel;
-                    });
+                    var newOptionLabel = UnityEngine.Object.Instantiate(optionLabel, optionLabel.transform.parent, false);
+                    newOptionLabel.name = x.ToString();
+                    newOptionLabel.text = x.ToString();
+                    return newOptionLabel;
+                });
 
-                    ___maxPlayersSelector.optionLabels = ___maxPlayersSelector.optionLabels.Concat(newLabels).ToArray();
-                    right.SetParent(optionLabel.transform.parent);
-                }
-
-                return true;
+                ___maxPlayersSelector.optionLabels = ___maxPlayersSelector.optionLabels.Concat(newLabels).ToArray();
+                right.SetParent(optionLabel.transform.parent);
             }
+
+            return true;
         }
     }
 }
